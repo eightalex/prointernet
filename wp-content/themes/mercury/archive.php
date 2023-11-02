0711f335962633a3f380eefe52d1f46be423e677
eightalex <eight.alex@gmail.com>
2023-11-02 22:36:12 +0200

    Fixed tabs

--- wp-content/themes/mercury/archive.php
+++ wp-content/themes/mercury/archive.php
@@ -3,17 +3,17 @@
 <!-- Title Box Start -->
 
 <div class="space-archive-title-box box-100 relative">
-	<div class="space-archive-title-box-ins space-page-wrapper relative">
-		<div class="space-archive-title-box-h1 relative">
-			<h1><?php the_archive_title( '', '' );?></h1>
-			
-			<!-- Breadcrumbs Start -->
+    <div class="space-archive-title-box-ins space-page-wrapper relative">
+        <div class="space-archive-title-box-h1 relative">
+            <h1><?php the_archive_title( '', '' );?></h1>
 
-			<?php get_template_part( '/theme-parts/breadcrumbs' ); ?>
+            <!-- Breadcrumbs Start -->
 
-			<!-- Breadcrumbs End -->
-		</div>
-	</div>
+            <?php get_template_part( '/theme-parts/breadcrumbs' ); ?>
+
+            <!-- Breadcrumbs End -->
+        </div>
+    </div>
 </div>
 
 <!-- Title Box End -->
@@ -21,13 +21,13 @@
 <!-- Archive Section Start -->
 
 <div class="space-archive-section box-100 relative">
-	<div class="space-archive-section-ins space-page-wrapper relative">
-		<div class="space-content-section box-75 left relative">
-			<div class="box-100 relative">
-			    
-			    <div class="space-page-content-wrap">
-			        <div class="space-page-content">
-			            <?php echo do_shortcode( '[custom-archive-toc]' ); ?>
+    <div class="space-archive-section-ins space-page-wrapper relative">
+        <div class="space-content-section box-75 left relative">
+            <div class="box-100 relative">
+
+                <div class="space-page-content-wrap">
+                    <div class="space-page-content">
+                        <?php echo do_shortcode( '[custom-archive-toc]' ); ?>
                     </div>
                 </div>
 
@@ -37,51 +37,51 @@
                     </div>
                 <?php } ?>
 
-			</div>
-			<div class="space-archive-loop box-100 relative">
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            </div>
+            <div class="space-archive-loop box-100 relative">
+                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( '/theme-parts/archive/loop' ); ?>
+                <?php get_template_part( '/theme-parts/archive/loop' ); ?>
 
-				<?php endwhile; ?>
+                <?php endwhile; ?>
 
-				<!-- Archive Navigation Start -->
+                <!-- Archive Navigation Start -->
 
-				<?php
-					the_posts_pagination( array(
-						'end_size' => 2,
-						'prev_text'    => esc_html__('&laquo;', 'mercury'),
-						'next_text'    => esc_html__('&raquo;', 'mercury'),
-					));
-				?>
+                <?php
+                    the_posts_pagination( array(
+                        'end_size' => 2,
+                        'prev_text'    => esc_html__('&laquo;', 'mercury'),
+                        'next_text'    => esc_html__('&raquo;', 'mercury'),
+                    ));
+                ?>
 
-				<!-- Archive Navigation End -->
+                <!-- Archive Navigation End -->
 
-				<?php else : ?>
+                <?php else : ?>
 
-				<!-- Posts not found Start -->
+                <!-- Posts not found Start -->
 
-				<div class="space-page-content-wrap relative">
-					<div class="space-page-content page-template box-100 relative">
-						<h2><?php esc_html_e( 'Posts not found', 'mercury' ); ?></h2>
-						<p>
-							<?php esc_html_e( 'No posts has been found. Please return to the homepage.', 'mercury' ); ?>
-						</p>
-					</div>
-				</div>
+                <div class="space-page-content-wrap relative">
+                    <div class="space-page-content page-template box-100 relative">
+                        <h2><?php esc_html_e( 'Posts not found', 'mercury' ); ?></h2>
+                        <p>
+                            <?php esc_html_e( 'No posts has been found. Please return to the homepage.', 'mercury' ); ?>
+                        </p>
+                    </div>
+                </div>
 
-				<!-- Posts not found End -->
+                <!-- Posts not found End -->
 
-				<?php endif; ?>
+                <?php endif; ?>
 
-			</div>
-		</div>
-		<div class="space-sidebar-section box-25 left relative">
+            </div>
+        </div>
+        <div class="space-sidebar-section box-25 left relative">
 
-			<?php get_sidebar(); ?>
+            <?php get_sidebar(); ?>
 
-		</div>
-	</div>
+        </div>
+    </div>
 </div>
 
 <!-- Archive Section End -->
