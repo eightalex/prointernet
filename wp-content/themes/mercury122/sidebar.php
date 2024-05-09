ffa7f0f37e48b23b1f952e160e5d8b5925e6ba31
eightalex <eight.alex@gmail.com>
2024-05-09 21:22:28 +0300

    Removed unnecessary theme

--- wp-content/themes/mercury122/sidebar.php
+++
@@ -1,5 +0,0 @@
-	<?php
-		if ( is_active_sidebar( 'right-sidebar' ) ) {
-			dynamic_sidebar( 'right-sidebar' );
-		}
-	?>
--- wp-content/themes/mercury122/theme-parts/breadcrumbs.php
+++
@@ -1,5 +0,0 @@
-<?php if ( function_exists('yoast_breadcrumb') ) { ?>
-	<div class="space-single-aces-breadcrumbs relative">
-		<?php yoast_breadcrumb('<div class="space-breadcrumbs relative">','</div>'); ?>
-	</div>
-<?php } ?>
